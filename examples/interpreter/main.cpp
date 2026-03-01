4d11662d0b0dbb989cb2351608738d71d4200048
dumitrioo <dmitryy@mailfence.com>
2026-03-01 08:40:46 +0200

    useful example...

--- examples/interpreter/main.cpp
+++ examples/interpreter/main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <thread>
 #include <filesystem>
+#include <iostream>
 
 #include <scaflux_runtime.hpp>
 
@@ -12,12 +13,61 @@
 #include "optext/ray_ext.hpp"
 #endif
 
+class example_object {
+public:
+    example_object() = default;
+    example_object(int v): v_{v} {}
+    void set_val(int v) { v_ = v; }
+    int get_val() const { return v_; }
+
+private:
+    int v_{};
+};
+
 int main(int argc, char **argv) {
     std::vector<std::string> args{argv, argv + argc};
     std::setlocale(LC_ALL, "en_US.UTF-8");
 
+    if(args.size() == 1) {
+        return 0;
+    }
+
     scfx::runtime rt{};
 
+    // example of adding a function to the runtime
+    rt.add_function("hello_from_cpp", [](scfx::valbox const &/*fname*/, std::vector<scfx::valbox> &args) -> scfx::valbox {
+        std::cout << "C++ extension function hello_from_cpp() called" << std::endl
+                  << "the function arguments:" << std::endl;
+        for(auto &&a: args) {
+            std::cout << "\t" << a << std::endl;
+        }
+        return args.size();
+    });
+    // ----------------------------------------------
+
+    // example of adding a named value to the runtime
+    rt.add_var("The_Answer_to_the_Ultimate_Question_of_Life_the_Universe_and_Everything", 42);
+    // ----------------------------------------------
+
+    // example of adding an object type to the runtime
+    rt.add_function("example_object", [](scfx::valbox const &/*fname*/, std::vector<scfx::valbox> &args) -> scfx::valbox {
+        if(args.size() > 0) {
+            return scfx::valbox{example_object{args[0].cast_to_s32()}, "example_object"};
+        }
+        return scfx::valbox{example_object{}, "example_object"};
+    });
+    rt.add_method("example_object", "set_val", SCFXFUN(/*fname*/, args) {
+        SCFX_CHCK_FUN_PARMS_NUM_EQ(2) // check number of arguments, if needed, including implicit object reference as the first arg
+        SCFXTHIS(example_object).set_val(args[1].cast_to_s32());
+        return 0;
+    });
+    rt.add_method("example_object", "get_val", SCFXFUN(/*fname*/, args) {
+        SCFX_CHCK_FUN_PARMS_NUM_EQ(1)
+        return SCFXTHIS(example_object).get_val();
+    });
+    // ----------------------------------------------
+
+    // examples of adding an extension to the runtime
 #ifdef SCFX_USE_ZMQ
     zmq_ext zmq{};
     zmq.register_runtime(&rt);
@@ -26,10 +76,7 @@ int main(int argc, char **argv) {
     ray_ext ray{};
     ray.register_runtime(&rt);
 #endif
-
-    if(args.size() == 1) {
-        return 0;
-    }
+    // ----------------------------------------------
 
 #ifndef DEBUG_SCFX_RUN_CYCLE
     try {
