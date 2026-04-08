6378074d3a077356734b4b7d36bb8e35baf809a6
dumitrioo <dmitryy@mailfence.com>
2026-04-08 23:00:48 +0300

    version

--- src/tealscript_runtime.hpp
+++ src/tealscript_runtime.hpp
@@ -2500,7 +2500,7 @@ namespace teal {
         std::list<std::pair<std::shared_ptr<so>, extension_interface *>> loaded_extensions_{};
         static std::size_t constexpr version_major_{1};
         static std::size_t constexpr version_minor_{4};
-        static std::size_t constexpr version_patch_{15};
+        static std::size_t constexpr version_patch_{19};
 
 #ifdef TEAL_USE_EXTERNAL_VALUES
 
